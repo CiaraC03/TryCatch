1b72e390e586661cd3d76893863cf943eda7d22a
G00402357 <g00402357@atu.ie>
2022-11-08 14:18:08 +0000

    New code added with catch and loop

--- src/main/java/ie/atu/week7.java
+++ src/main/java/ie/atu/week7.java
@@ -8,21 +8,29 @@ public class week7
     {
         System.out.println("Please enter a number between 0 - 9 : ");
         Scanner myscan = new Scanner(System.in);
+
+       while(true){
         try{
             int myNum = myscan.nextInt();
             if (myNum <= 9 && myNum > 0)
-                {
+            {
                 System.out.println("You entered " + myNum);
+                break;
 
-                }
+            }
             else{
                 System.out.println("Not a valid number");
-                }
+                System.out.println("Please enter an integer from 1 to 9 : ");
             }
-            catch(InputMismatchException h)
-            {
-                System.out.println("Error caught");
-            }
+        }
+        catch(InputMismatchException h)
+        {
+            System.out.println("Error caught");
+            myscan.next();
+            System.out.println("Pleas enter an integer from 1 to 9 : ");
+        }
+
+    }
 
 
     }
